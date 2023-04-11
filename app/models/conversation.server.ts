aee0eb4d7d27ff32f7c5aac67c254354b0fe266a
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-04-11 22:43:31 +0200

    read conversation list from db

--- app/models/conversation.server.ts
+++ app/models/conversation.server.ts
@@ -1,97 +1,94 @@
-type Conversation = {
-  level: string;
-  course: string;
-  lesson: string;
-  sentences: Array<Sentence>;
-};
+import { User } from "@prisma/client";
 
-type Sentence = {
-  id: string;
-  order: string;
-  type: string;
-  text: string;
-};
+import { prisma } from "~/db.server";
 
-export async function getConversations(): Promise<Array<Conversation>> {
-  return [
-    {
-      level: "A1",
-      course: "2",
-      lesson: "14",
-      sentences: [
-        {
-          id: "1",
-          order: "1",
-          type: "Description",
-          text: "Es ist beinah an der Zeit für einen großen Sport-Wettkamp. Ren und Aida versuchen, online Tickets zu bekommen...",
-        },
-        {
-          id: "2",
-          order: "2",
-          type: "PersonA",
-          text: "What do you want to see, Aida? Do you like swimming?",
-        },
-        {
-          id: "3",
-          order: "3",
-          type: "PersonB",
-          text: "Yes, I love swimming. It's my favorite sport.",
-        },
-        {
-          id: "4",
-          order: "4",
-          type: "PersonA",
-          text: "Great, me too!",
-        },
-        {
-          id: "5",
-          order: "5",
-          type: "Description",
-          text: "Ren versucht, Tickets für Schwimmen zu bekommen, aber sie sind schon alle weg...",
-        },
-        {
-          id: "6",
-          order: "6",
-          type: "PersonB",
-          text: "Oh no! We can't watch swimming. Do you like basketball?",
-        },
-        {
-          id: "7",
-          order: "7",
-          type: "PersonA",
-          text: "Umm, no, I don't like basketball.",
-        },
-        {
-          id: "8",
-          order: "8",
-          type: "PersonB",
-          text: "Do you like watching tennis?",
-        },
-        {
-          id: "9",
-          order: "9",
-          type: "PersonA",
-          text: "Yes, I like watching tennis.",
-        },
-        {
-          id: "10",
-          order: "10",
-          type: "Description",
-          text: "Die beiden versuchen, Tickets für Tennis zu reservieren...",
-        },
-        {
-          id: "11",
-          order: "11",
-          type: "PersonB",
-          text: "We got the last two tickets!",
-        },
-        {
-          id: "12",
-          order: "12",
-          type: "PersonA",
-          text: "Yes, I'm so excited!",
-        },
-      ],
-    },
-  ];
+export function getConversationListItems({ userId }: { userId: User["id"] }) {
+  return prisma.conversation.findMany({
+    where: { userId },
+    select: { id: true, level: true, course: true, lesson: true },
+    orderBy: { createdAt: "desc" },
+  });
 }
+// export async function getConversations(): Promise<Array<Conversation>> {
+//   return [
+//     {
+//       level: "A1",
+//       course: "2",
+//       lesson: "14",
+//       sentences: [
+//         {
+//           id: "1",
+//           order: "1",
+//           type: "Description",
+//           text: "Es ist beinah an der Zeit für einen großen Sport-Wettkamp. Ren und Aida versuchen, online Tickets zu bekommen...",
+//         },
+//         {
+//           id: "2",
+//           order: "2",
+//           type: "PersonA",
+//           text: "What do you want to see, Aida? Do you like swimming?",
+//         },
+//         {
+//           id: "3",
+//           order: "3",
+//           type: "PersonB",
+//           text: "Yes, I love swimming. It's my favorite sport.",
+//         },
+//         {
+//           id: "4",
+//           order: "4",
+//           type: "PersonA",
+//           text: "Great, me too!",
+//         },
+//         {
+//           id: "5",
+//           order: "5",
+//           type: "Description",
+//           text: "Ren versucht, Tickets für Schwimmen zu bekommen, aber sie sind schon alle weg...",
+//         },
+//         {
+//           id: "6",
+//           order: "6",
+//           type: "PersonB",
+//           text: "Oh no! We can't watch swimming. Do you like basketball?",
+//         },
+//         {
+//           id: "7",
+//           order: "7",
+//           type: "PersonA",
+//           text: "Umm, no, I don't like basketball.",
+//         },
+//         {
+//           id: "8",
+//           order: "8",
+//           type: "PersonB",
+//           text: "Do you like watching tennis?",
+//         },
+//         {
+//           id: "9",
+//           order: "9",
+//           type: "PersonA",
+//           text: "Yes, I like watching tennis.",
+//         },
+//         {
+//           id: "10",
+//           order: "10",
+//           type: "Description",
+//           text: "Die beiden versuchen, Tickets für Tennis zu reservieren...",
+//         },
+//         {
+//           id: "11",
+//           order: "11",
+//           type: "PersonB",
+//           text: "We got the last two tickets!",
+//         },
+//         {
+//           id: "12",
+//           order: "12",
+//           type: "PersonA",
+//           text: "Yes, I'm so excited!",
+//         },
+//       ],
+//     },
+//   ];
+// }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,14 +4,14 @@ import bcrypt from "bcryptjs";
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const email = "ehsan@tatasadi.com";
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const hashedPassword = await bcrypt.hash("remixiscool", 10);
 
   const user = await prisma.user.create({
     data: {
@@ -24,19 +24,110 @@ async function seed() {
     },
   });
 
-  await prisma.note.create({
+  const conversation = await prisma.conversation.create({
     data: {
-      title: "My first note",
-      body: "Hello, world!",
+      level: "A1",
+      course: 2,
+      lesson: 14,
       userId: user.id,
     },
   });
 
-  await prisma.note.create({
+  await prisma.sentence.create({
     data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
+      order: 1,
+      type: "Description",
+      text: "Es ist beinah an der Zeit für einen großen Sport-Wettkamp. Ren und Aida versuchen, online Tickets zu bekommen...",
+      conversationId: conversation.id,
+    },
+  });
+
+  await prisma.sentence.create({
+    data: {
+      order: 2,
+      type: "PersonA",
+      text: "What do you want to see, Aida? Do you like swimming?",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 3,
+      type: "PersonB",
+      text: "Yes, I love swimming. It's my favorite sport.",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 4,
+      type: "PersonA",
+      text: "Great, me too!",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 5,
+      type: "Description",
+      text: "Ren versucht, Tickets für Schwimmen zu bekommen, aber sie sind schon alle weg...",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 6,
+      type: "PersonB",
+      text: "Oh no! We can't watch swimming. Do you like basketball?",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 7,
+      type: "PersonA",
+      text: "Umm, no, I don't like basketball.",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 8,
+      type: "PersonB",
+      text: "Do you like watching tennis?",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 9,
+      type: "PersonA",
+      text: "Yes, I like watching tennis.",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 10,
+      type: "Description",
+      text: "Die beiden versuchen, Tickets für Tennis zu reservieren...",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 11,
+      type: "PersonB",
+      text: "We got the last two tickets!",
+      conversationId: conversation.id,
+    },
+  });
+  await prisma.sentence.create({
+    data: {
+      order: 12,
+      type: "PersonA",
+      text: "Yes, I'm so excited!",
+      conversationId: conversation.id,
     },
   });
 
